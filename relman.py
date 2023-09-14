d656c077149951cda1688d2cee825350212ddbc8
AGARWAL Harsh Mohan (INTERN) <HAL5@dsone.3ds.com>
2023-09-14 13:26:03 -0400

    Support for commit msg with emoji added

--- relman.py
+++ relman.py
@@ -291,6 +291,18 @@ class Changelog(object):
         changelog_entries = '\n'.join(self.get_changelog_entries(previous, 'HEAD', self.releasable.path))
         date = datetime.date.today().strftime('%Y-%m-%d')
         return self.CHANGELOG_FMT.format(repo_url=repo_url, current=current, previous=previous, changelog_entries=changelog_entries, date=date)
+    
+    @classmethod
+    def removeEmoji(self, str):
+        emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+        return emoji_pattern.sub(r'', str)
 
     @classmethod
     def get_changelog_entries(cls, ref1, ref2, *files):
@@ -302,6 +314,7 @@ class Changelog(object):
             m = re.match(cls.RE_COMMIT_MSG, msg)
             if m:
                 msg = m.group(1)
+                msg = cls.removeEmoji(msg)
                 pr = m.group(2)
                 changelog_entries.append(cls.CHANGELOG_ENTRY_FMT.format(cls.get_repo_url(), sha, msg, pr))
             else:
